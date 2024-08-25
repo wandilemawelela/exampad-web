b24222a734db074c72c688c5e853552fb5556d5e
wandilemawelela <wandile1997@gmail.com>
2024-08-25 07:22:16 +0200

    [ADD] Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+  ],
   theme: {
     extend: {},
   },
